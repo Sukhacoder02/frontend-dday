466b734966123a9f3982c7e6ce822b852d6ca8d8
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-03-10 02:52:58 +0530

    feat: add component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './Header.css';
+interface HeaderProps {
+  title: string;
+}
+const Header: React.FC<HeaderProps> = (props: HeaderProps): JSX.Element => {
+  return (
+    <header>
+      <div className="header-title">
+        <h2>{props.title}</h2>
+      </div>
+    </header>
+  );
+};
+export default Header;
