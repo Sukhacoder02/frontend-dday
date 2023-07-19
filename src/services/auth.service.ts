1256c9796b922ac18b74649b5f5aeedbddff35d5
Sukhman S <sukhmansindhipb05@gmail.com>
2023-07-19 12:28:45 +0530

    (refactor): refactor login and signup services

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,28 +1,17 @@
 import axios from 'axios';
 
 const login = async (email: string, password: string) => {
-  return axios
-    .post('http://localhost:1111/api/login', {
-      email: email,
-      password: password,
-    })
-    .then(response => {
-      if (response.data.token) {
-        console.log('response.data.token', response.data.token);
-        localStorage.setItem('user', JSON.stringify(response.data));
-      }
-    });
+  return axios.post('http://localhost:1111/api/login', {
+    email: email,
+    password: password,
+  });
 };
 
 const signup = async (email: string, password: string) => {
-  return axios
-    .post('http://localhost:1111/api/register', {
-      email: email,
-      password: password,
-    })
-    .then(response => {
-      console.log(response);
-    });
+  return axios.post('http://localhost:1111/api/register', {
+    email: email,
+    password: password,
+  });
 };
 
 export { login, signup };
